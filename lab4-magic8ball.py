9a2c2f6d1017a2069e58f10cd39a7fcdee764510
brukeg <bruke@spreadlabel.com>
2019-02-01 19:53:51 -0800

    finalizing lab4.

--- lab4-magic8ball.py
+++ lab4-magic8ball.py
@@ -9,5 +9,42 @@ Write a program to simulate the classic "Magic Eight Ball
 5. Using a while loop, keep asking the user for a question, if they enter 'done', exit
 """
 
-while True
-#lol
+import random
+
+predictions = [
+		"It is certain",
+		"It is decidedly so",
+		"Without a doubt",
+		"Yes definitely",
+		"You may rely on it",
+		"As I see it, yes",
+		"Most likely",
+		"Outlook good",
+		"Yes",
+		"Signs point to yes",
+		"Reply hazy try again",
+		"Ask again later",
+		"Better not tell you now",
+		"Cannot predict now",
+		"Concentrate and ask again",
+		"Don't count on it",
+		"My reply is no",
+		"My sources say no",
+		"Outlook not so good",
+		"Very doubtful"
+	]
+
+print("Welcome to the fucking MAGIC 8-ball of glory!!")
+input("Ask your question wisely: ").strip().lower()
+reply = (random.choice(predictions))
+print(reply)
+
+while True:
+	keep_playing = input("You may ask another question or enter DONE: ").strip().lower()
+	if keep_playing == 'done':
+		print("Goodbye!")
+		break
+	else:
+		keep_playing
+		print(reply)
+
