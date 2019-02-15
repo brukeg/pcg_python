65879773cca94142100855f71920cde1b584d0e2
brukeg <bruke@spreadlabel.com>
2019-02-14 21:15:47 -0800

    ffs, it's done through v2

--- lab15-number-to-phrase.py
+++ lab15-number-to-phrase.py
@@ -12,41 +12,55 @@ Version 4 (optional)
 Convert a time given in hours and minutes to a phrase.
 
 """
-print(10 % 100)
-print(11 % 100)
-print(67 % 10)
-print(10 // 10)
-print(11 // 10)
-print(67 // 10)
-
-ones = {0: 'zero', 1: 'one', 2: 'two', 3: 'three', 4: 'four', 5: 'five', 6: 'six', 7: 'seven', 8: 'eight', 9: 'nine'}
-teens ={0: 'ten', 1: 'eleven', 2: 'twelve', 3: 'thirteen', 4: 'fourteen', 5: 'fifteen', 6: 'sixteen', 7: 'seventeen',8: 'eighteen', 9: 'nineteen'}
-tens = {2: 'twenty', 3: 'thirty', 4: 'fourty', 5: 'fifty', 6: 'sixty', 7: 'sevnty', 8: 'eight', 9: 'ninety'}
+unos = ['zero', 'one', 'two', 'three', 'four', 'five', 'six', 'seven', 'eight', 'nine']
+teens = ['ten', 'eleven', 'twelve', 'thirteen', 'fourteen', 'fifteen', 'sixteen', 'seventeen', 'eighteen', 'nineteen']
+tens = ['twenty', 'thirty', 'fourty', 'fifty', 'sixty', 'sevnty', 'eight', 'ninety']
+hundo = 'hundred'
 
 
 def number_to_phrase(integer):
     """
     return a english word representation of a integer between 0-99
     """
-    if integer > 19 and ones_digit != 'zero':
-            tens_digit = ''
-            for key in tens:
-                if integer // 10 == key:
-                    tens_digit += tens[integer // 10]
+    if integer > 99:
+        """
+        Handle numbers over 99
+        """
+        num = ''
+        if integer % 100 == 0:
+            num += str(unos[integer // 100]) + '-' + hundo # hundreds
+        if integer % 100 < 10 and integer % 100 != 0:
+            num += str(unos[integer // 100]) + '-' + hundo + '-' + unos[integer % 100] # hundred-ones
+        if (integer % 100) // 10 == 1:
+            num += str(unos[integer // 100]) + '-' + hundo + '-' + teens[(integer % 100) % 10] # hundred-ten/teens
+        if integer % 100 > 19:
+            if (integer % 100) % 10  == 0:
+                num += str(unos[integer // 100]) + '-' + hundo + '-' + tens[(((integer % 100) // 10) - 2)] # hundred-tens
+            else:
+                num += str(unos[integer // 100]) + '-' + hundo + '-' + tens[(((integer % 100) // 10) - 2)] + '-' + unos[((integer % 100) % 10)] # one-hundred-tens-ones
+        return num
+
+    if integer > 19:
+        """ 
+        Handle any number over 19
+        """
+        num = ''
+        if integer % 10 == 0:
+            num += tens[(integer // 10) - 2]
+        else:
+            num = str(tens[(integer // 10) - 2]) + '-' + (str(unos[integer % 10]))
+        return num
         
-        if integer // 10 == 1:
-            teenth = ''
-            for key in teens:
-                if integer % 10 == key:
-                    teenth += teens[integer % 10]
-            
-            if integer < 10 or integer > 19:
-                ones_digit = ''
-                for key in ones:
-                    if integer % 10 == key:
-                        ones_digit += ones[integer % 10]
+    if integer // 10 == 1:
+        """
+        Handle the teens explicitly
+        """
+        return teens[integer % 10]
 
-            return tens_digit + '-' + ones_digit
+    if integer < 10:
+        """
+        Handle single number digits
+        """
+        return unos[integer]
 
-
-print(number_to_phrase(21))
+print(number_to_phrase(921))
