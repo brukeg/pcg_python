e9e35923e3205cb779b9ca39bf4b8283dad5d9a6
brukeg <bruke@spreadlabel.com>
2019-02-19 18:43:01 -0800

    finished lab20

--- lab20-credit-card-validation.py
+++ lab20-credit-card-validation.py
@@ -14,14 +14,27 @@ Write a function which returns whether a string containing a credit card number 
 credit_card = '4 5 5 6 7 3 7 5 8 6 8 9 9 8 5 5'
 def ccv(number_string):
 	string_to_list = number_string.split(' ')
-	check_digit = int(string_to_list[-1])
+	check_digit = string_to_list[-1]
 	slice_off = ''.join(string_to_list.pop())
 	reverse = string_to_list[::-1]
 	reverse[::2] = [int(n)*2 for n in reverse[::2]]
+	minus_nine = []
+	addition = 0
 	
-	return reverse
+	for i in reverse:
+		if int(i) > 9:
+			minus_nine.append(int(i) - 9)
+		else:
+			minus_nine.append(int(i))
 	
-
+	for i in minus_nine:
+		addition += i
+	first_index = str(addition)[1]
+	
+	if first_index == check_digit:
+		return "Valid!"
+	else:
+		return "Not valid"
 
 print(ccv(credit_card))
 
