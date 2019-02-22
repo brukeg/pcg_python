d1636b3fc4445bd1bf4047528a4ab1fed7645860
brukeg <bruke@spreadlabel.com>
2019-02-21 19:12:43 -0800

    checking in near finished lab23

--- lab23-contact-list.py
+++ lab23-contact-list.py
@@ -11,8 +11,6 @@ represent the values.
 with open('contacts.csv', 'r') as file:
     lines = file.read().split('\n')
 
-
-
 contacts = []
 keys = lines[0].split(',')
 
@@ -20,5 +18,47 @@ for i in range(1, len(lines)):
 	entry = lines[i].split(',')
 	entry = dict(zip(keys, entry))
 	contacts.append(entry)
-	
-print(contacts)
+
+"""
+Create a record: ask the user for each attribute, 
+add a new contact to your contact list with the attributes that the user entered.
+"""
+name = input("Enter a name: ")
+phone = input("Enter a phone number: ")
+city = input("Enter the city name: ")
+
+contacts.append({'name': name, 'phone': phone, 'city': city})
+
+
+"""
+Retrieve a record: ask the user for the contact's name, 
+find the user with the given name, and display their information
+"""
+retriever =  input("What is the contacts name?: ")
+for i in contacts:
+	if i["name"] == retriever:
+		print(i)
+
+"""
+Update a record: ask the user for the contact's name, then for which attribute of the 
+user they'd like to update and the value of the attribute they'd like to set.
+"""
+contact_update = input("What's the contact's name?: ")
+attribute = input("Which attribute do you want to update (name, phone, city)?: ")
+attribute_value = input("What should it be replaced by?: ")
+
+for i in contacts:
+	if i["name"] == contact_update:
+		i[attribute] = attribute_value
+		print(i)
+
+
+"""
+Delete a record: ask the user for the contact's name, 
+remove the contact with the given name from the contact list.
+"""
+
+del_contact = input("Which contact do you want to remove?: ")
+for index, i in enumerate(contacts):
+	if i["name"] == del_contact:
+		del contacts[index]
